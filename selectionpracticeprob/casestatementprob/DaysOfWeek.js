f45fd6706791153ec5d9c6b115fca28c98380ca9
mangesh9903 <mangeshb9903@gmail.com>
2022-05-02 04:05:27 +0530

    ADDED: Feature to Check Days Of Week.

---
+++ selectionpracticeprob/casestatementprob/DaysOfWeek.js
@@ -0,0 +1,31 @@
+// To read a number and display the week day
+
+const prompt = require('prompt-sync')();
+
+let weekDayNumber = Number(prompt("Enter number : "));
+
+switch (weekDayNumber) {
+    case 1:
+        console.log("Sunday");
+        break;
+    case 2:
+        console.log("Monday");
+        break;
+    case 3:
+        console.log("Tuesday");
+        break;
+    case 4:
+        console.log("Wednesday");
+        break;
+    case 5:
+        console.log("Thursday");
+        break;
+    case 6:
+        console.log("Friday");
+        break;
+    case 7:
+        console.log("Saturday");
+        break;
+    default:
+        console.log("Invalid!!");
+}
